f379eba73ca19620116832e085c441171fe4569e
KrzysztofCiemny <Ciemny.Krzysztof97@gmail.com>
2021-06-21 12:24:55 +0200

    added mechanic for drop down services

--- services.js
+++ services.js
@@ -1,7 +1,9 @@
 const services = document.querySelector('.services');
+const arrowIcon = document.querySelector('.icon-down-open-mini');
 
 const dropDownServices = () => {
     services.classList.toggle('services-active');
+    arrowIcon.classList.toggle('icon-rotate');
 }
 
 export default dropDownServices;
