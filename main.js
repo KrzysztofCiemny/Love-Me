dcbd2c343034e0403891fb0177d91abf2b391946
KrzysztofCiemny <Ciemny.Krzysztof97@gmail.com>
2021-07-10 02:00:18 +0200

    added scroll to element form navLinks mechanic

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 import hamburgerNavClick from "./hamburgerClick.js";
 import dropDownServices from "./services.js";
-import {scrollToTop, scrollToElement} from "./scrollToTop.js";
+import scrollTo from "./scrollToTop.js";
 import slider from "./slider.js";
 
 const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', hamburgerNavClick);
 
-
 dropDownServices();
 slider();
-
-const aboutUsLink = document.querySelector('#aboutUsLink');
-aboutUsLink.addEventListener('click', scrollToElement)
-
-const scrollToTopBtn = document.querySelector('.scrollUp');
-scrollToTopBtn.addEventListener('click', scrollToTop);
+scrollTo();
--- scrollToTop.js
+++ scrollToTop.js
@@ -1,17 +1,32 @@
-// const aboutUsElement = document.querySelector('#aboutUsLink');
-const aboutUs = document.querySelector('.AboutUs-Container');
+const navLinks = document.querySelectorAll('.nav_item');
+const elements = document.querySelectorAll('.scroll-to-element');
+const scrollToTopBtn = document.querySelector('.scrollUp');
 
-export function scrollToElement() {
-    aboutUs.scrollIntoView({
-        behavior: "smooth"
-    });
+console.log(elements);
+
+
+function scrollTo() {
+    navLinks.forEach(navLink => navLink.addEventListener('click', scrollToElement));
+
+    function scrollToElement(e) {
+        const navLinkClicked = e.target;
+
+        elements.forEach(element => {
+            if(element.dataset.scrollTo === navLinkClicked.dataset.navIndex) {
+                element.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+        });
+    }
+
+    scrollToTopBtn.addEventListener('click', scrollToTop);
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
 }
-
-export function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    });
-}
-
+export default scrollTo;
