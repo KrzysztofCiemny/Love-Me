d146880d72b004a618ae0e42e63475e348c7aed0
KrzysztofCiemny <ciemnykrzysztof97@gmail.com>
2021-09-30 02:00:22 +0200

    added function for animations on scroll

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import hamburgerNavClick from "./hamburgerClick.js";
 import dropDownServices from "./services.js";
 import scrollTo from "./scrollTo.js";
 import slider from "./slider.js";
-import scrollAnimation from "./scrollAnimations.js";
+import scrollAnimations from "./scrollAnimations.js";
 
 hamburgerNavClick();
 dropDownServices();
 slider();
 scrollTo();
-scrollAnimation();
+scrollAnimations();
